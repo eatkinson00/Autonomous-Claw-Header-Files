915238d7df432ad41e2625adfcd323ec2284939e
eatkinson00 <36234566+eatkinson00@users.noreply.github.com>
2018-03-30 00:02:50 -0400

    Create main.h

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef AUTONOMOUS_CLAW_H
+#define AUTONOMOUS_CLAW
+
+#define steps_per_rev   20
+#define xPin1   22
+#define xPin2   24
+#define xPin3   26
+#define xPin4   28
+#define yPin1   30
+#define yPin2   32
+#define yPin3   34
+#define yPin4   36
+#define zPin1   38
+#define zPin2   40
+#define zPin3   42
+#define zPin4   44
+
+#endif 
